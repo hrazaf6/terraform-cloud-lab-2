058284107af67b074dafdd6b246e3c375fd89066
Hamid Raza <hamidraza@Hamids-MacBook-Pro.local>
2020-07-04 19:37:39 +0530

    added variable vpc_cidr

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,13 @@
 module "create_vpc" {
     source = "./modules/networking"
-    vpc_cidr = "172.40.0.0/16"
+    vpc_cidr = var.vpc_cidr
     public_subnet_names = ["Pub1", "Pub2", "Pub3"]
 }
 
 output "vpc_id_root_module" {
     value = module.create_vpc.vpc_id
+}
+
+variable "vpc_cidr" {
+    default = "172.40.0.0/16"
 }
